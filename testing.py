7822c350b7ad27769acba5e5d639ef5be94f4981
Benjamin Beuster <benjamin.beuster@sikt.no>
2024-04-29 11:43:17 +0200

    Unnecessary files removed from the repository

--- testing.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-import pandas as pd
-df = pd.read_parquet("https://ess-api.sikt.no/v1/data/10.21338/ess9e03_2?user_id=1234c034-4ff9-11e5-885d-feff819cdc9f")
-df.head()
-
-
